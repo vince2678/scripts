da215804a584f8513b7dcdd3a42c6d0ebc9e4d59
Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
2017-02-11 15:28:23 +0000

    Fix platform detection

--- run_build.sh
+++ run_build.sh
@@ -162,8 +162,8 @@ function main {
 	#get the platform version
 	platorm_version=$(grep 'PLATFORM_VERSION :' build/core/version_defaults.mk  | cut -d '=' -f 2)
 	export WITH_SU=true
-	if [ "$platorm_version" == "7.1.1" ]; then
-	export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+	if [ $platorm_version == "7.1.1" ]; then
+		export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 		if [ "$distro" == "lineage" ]; then
 			ver="14.1"
 			distroTxt="LineageOS"
