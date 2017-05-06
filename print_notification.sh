e7303c73cb645cd585046caad5a2e3f1a75205df
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-06 05:25:00 -0400

    Just don't print the file paths anymore

--- print_notification.sh
+++ print_notification.sh
@@ -41,28 +41,9 @@ function print_end_build {
 	logb "Done."
 	if [ $silent -eq 0 ]; then
 		dateStr=`TZ='UTC' date +'[%H:%M:%S UTC]'`
-		target_str_len=$(echo ${BUILD_JENKINS_MOUNT_POINT} | wc -c)
-		link="${BUILD_URL}/artifact"
 
-		arc_name=${distro}-${ver}_j${build_num}_$(date +%Y%m%d)_${release_type}-${device_name}
-		rec_name=${recovery_flavour}-${distro}-${ver}_j${build_num}_$(date +%Y%m%d)_${device_name}
-		bimg_name=bootimage-${distro}-${ver}_j${build_num}_$(date +%Y%m%d)_${device_name}
-
-		if [ "$target" == "recoveryimage" ]; then
-			str_rec="%0ARecovery: ${link}/builds/recovery/${device_name}/${rec_name}.tar"
-		elif [ "$target" == "bootimage" ]; then
-			str_boot="%0ABoot image: ${link}/builds/boot/${device_name}/${bimg_name}.tar"
-		elif [ "$target" == "otapackage" ]; then
-			str_rom="%0A ROM: ${link}/builds/full/${arc_name}.zip"
-			if [ $ver == "13.0" ]; then
-				str_su="%0A SU: ${link}/builds/su/addonsu-arm_j${build_num}.zip"
-			fi
-			str_rec="%0A Recovery: ${link}/builds/recovery/${device_name}/${rec_name}.tar"
-		fi
 		str_main="${dateStr}[${target}] ${distroTxt} ${ver} build %23${build_num} for device ${device_name} on ${USER}@${HOSTNAME} completed successfully."
-		str_blurb="%0A%0AYou can flash boot/recovery images using ODIN or you can extract them using 7zip or tar under Linux and flash using TWRP."
-		str_changelog="%0AChangelog: ${link}/builds/full/${arc_name}.txt"
-		textStr="${str_main}${str_rom}${str_su}${str_rec}${str_boot}${str_changelog}${str_blurb}"
+		textStr="${str_main}"
 
 		wget "https://api.telegram.org/bot${BUILD_TELEGRAM_TOKEN}/sendMessage?chat_id=${BUILD_TELEGRAM_CHATID}&text=${textStr}" -O - > /dev/null 2>/dev/null
 	fi
