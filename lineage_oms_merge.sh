430d539b3f56bbbb01a0585473d25682192e5559
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-13 00:18:28 -0400

    Use spaces instead of tabs in oms script

--- lineage_oms_merge.sh
+++ lineage_oms_merge.sh
@@ -149,74 +149,74 @@ unset RESULT_STRING
 
 # START TRACKING TIME
 function sync_substratum() {
-	START=$( date +%s )
+    START=$( date +%s )
 
-	for FOLDER in ${SUBS_REPOS}; do
-	    # PRINT TO THE USER WHAT WE ARE DOING
-	    newLine; echoText "Merging ${FOLDER}"
+    for FOLDER in ${SUBS_REPOS}; do
+        # PRINT TO THE USER WHAT WE ARE DOING
+        newLine; echoText "Merging ${FOLDER}"
 
-	    # SHIFT TO PROPER FOLDER
-	    cd ${build_top}/${FOLDER}
+        # SHIFT TO PROPER FOLDER
+        cd ${build_top}/${FOLDER}
 
-	    # SET PROPER URL
-	    URL=android_$( echo ${FOLDER} | sed "s/\//_/g" )
+        # SET PROPER URL
+        URL=android_$( echo ${FOLDER} | sed "s/\//_/g" )
 
-	    BRANCH=cm-14.1
+        BRANCH=cm-14.1
 
-	    # FETCH THE REPO
-	    git fetch https://github.com/LineageOMS/${URL} ${BRANCH}
+        # FETCH THE REPO
+        git fetch https://github.com/LineageOMS/${URL} ${BRANCH}
 
-	    # GIT GYMNASTICS (GETS MESSY, BEWARE)
-	    # FIRST HASH WILL ALWAYS BE THE FETCH HEAD
-	    FIRST_HASH=$(git log --format=%H -1 FETCH_HEAD)
+        # GIT GYMNASTICS (GETS MESSY, BEWARE)
+        # FIRST HASH WILL ALWAYS BE THE FETCH HEAD
+        FIRST_HASH=$(git log --format=%H -1 FETCH_HEAD)
 
-	    # SECOND HASH WILL BE THE LAST THING I COMMITTED
-	    NUMBER_OF_COMMITS=$(( $( git log --format=%H --committer="Nathan Chancellor" FETCH_HEAD | wc -l ) - 1 ))
-	    SECOND_HASH=$( git log --format=%H --committer="Nathan Chancellor" FETCH_HEAD~${NUMBER_OF_COMMITS}^..FETCH_HEAD~${NUMBER_OF_COMMITS} )
+        # SECOND HASH WILL BE THE LAST THING I COMMITTED
+        NUMBER_OF_COMMITS=$(( $( git log --format=%H --committer="Nathan Chancellor" FETCH_HEAD | wc -l ) - 1 ))
+        SECOND_HASH=$( git log --format=%H --committer="Nathan Chancellor" FETCH_HEAD~${NUMBER_OF_COMMITS}^..FETCH_HEAD~${NUMBER_OF_COMMITS} )
 
-	    # NOW THAT WE HAVE THE HASHES, WE WANT TO TRY AND SEE IF OMS ALREADY EXISTS
-	    # THIS SCRIPT NEEDS TO BE RUN ON A CLEAN REPO
-	    SECOND_COMMIT_MESSAGE=$( git log --format=%s ${SECOND_HASH}^..${SECOND_HASH} | sed "s/\[\([^]]*\)\]/\\\[\1\\\]/g" )
-	    if [[ $( git log --format=%h --grep="${SECOND_COMMIT_MESSAGE}" --all-match | wc -l ) > 0 && ${URL} != "android" ]]; then
-		reportError "PREVIOUS COMMITS FOUND; SCRIPT MUST BE RUN ON A CLEAN REPO! EITHER REPO SYNC OR PICK COMMITS MANUALLY!" && exit_error $?
-	    fi
+        # NOW THAT WE HAVE THE HASHES, WE WANT TO TRY AND SEE IF OMS ALREADY EXISTS
+        # THIS SCRIPT NEEDS TO BE RUN ON A CLEAN REPO
+        SECOND_COMMIT_MESSAGE=$( git log --format=%s ${SECOND_HASH}^..${SECOND_HASH} | sed "s/\[\([^]]*\)\]/\\\[\1\\\]/g" )
+        if [[ $( git log --format=%h --grep="${SECOND_COMMIT_MESSAGE}" --all-match | wc -l ) > 0 && ${URL} != "android" ]]; then
+        reportError "PREVIOUS COMMITS FOUND; SCRIPT MUST BE RUN ON A CLEAN REPO! EITHER REPO SYNC OR PICK COMMITS MANUALLY!" && exit_error $?
+        fi
 
-	    # RESET ANY LOCAL CHANGES SO THAT CHERRY-PICK DOES NOT FAIL
-	    git reset --hard HEAD
-	    # PICK THE COMMITS IF EVERYTHING CHECKS OUT
-	    git cherry-pick ${SECOND_HASH}^..${FIRST_HASH}
+        # RESET ANY LOCAL CHANGES SO THAT CHERRY-PICK DOES NOT FAIL
+        git reset --hard HEAD
+        # PICK THE COMMITS IF EVERYTHING CHECKS OUT
+        git cherry-pick ${SECOND_HASH}^..${FIRST_HASH}
 
-	    # ADD TO RESULT STRING
-	    if [[ $? -ne 0 ]]; then
-		RESULT_STRING+="${FOLDER}: ${RED}FAILED${RESTORE}\n"
-	    else
-		RESULT_STRING+="${FOLDER}: ${GREEN}SUCCESS${RESTORE}\n"
-	    fi
-	done
+        # ADD TO RESULT STRING
+        if [[ $? -ne 0 ]]; then
+        RESULT_STRING+="${FOLDER}: ${RED}FAILED${RESTORE}\n"
+        else
+        RESULT_STRING+="${FOLDER}: ${GREEN}SUCCESS${RESTORE}\n"
+        fi
+    done
 
-	# SHIFT BACK TO THE TOP OF THE REPO
-	cd ${build_top}
+    # SHIFT BACK TO THE TOP OF THE REPO
+    cd ${build_top}
 
-	# SYNC THEME INTERFACER REPO
-	newLine; echoText "Syncing packages/services/ThemeInterfacer"
+    # SYNC THEME INTERFACER REPO
+    newLine; echoText "Syncing packages/services/ThemeInterfacer"
 
-	# Make sure that the local manifest exists
-	if [[ ! -f .repo/local_manifests/substratum.xml ]]; then
-	    mkdir -p .repo/local_manifests
-	    curl --silent --output .repo/local_manifests/substratum.xml \
-	    https://raw.githubusercontent.com/LineageOMS/merge_script/master/substratum.xml
-	fi
+    # Make sure that the local manifest exists
+    if [[ ! -f .repo/local_manifests/substratum.xml ]]; then
+        mkdir -p .repo/local_manifests
+        curl --silent --output .repo/local_manifests/substratum.xml \
+        https://raw.githubusercontent.com/LineageOMS/merge_script/master/substratum.xml
+    fi
 
-	repo sync --force-sync packages/services/ThemeInterfacer
+    repo sync --force-sync packages/services/ThemeInterfacer
 
-	# PRINT RESULTS
-	echoText "RESULTS"
-	echo -e ${RESULT_STRING}
+    # PRINT RESULTS
+    echoText "RESULTS"
+    echo -e ${RESULT_STRING}
 
-	# STOP TRACKING TIME
-	END=$( date +%s )
+    # STOP TRACKING TIME
+    END=$( date +%s )
 
-	# PRINT RESULT TO USER
-	echoText "SCRIPT COMPLETED!"
-	echo -e ${RED}"TIME: $(format_time ${END} ${START})"${RESTORE}; newLine
+    # PRINT RESULT TO USER
+    echoText "SCRIPT COMPLETED!"
+    echo -e ${RED}"TIME: $(format_time ${END} ${START})"${RESTORE}; newLine
 }
