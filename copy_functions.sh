84db9518768ac74c995c2e71569d0f84e78084f8
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-31 21:25:05 -0400

    Run OTA copy code only if build target is otapackage

--- copy_functions.sh
+++ copy_functions.sh
@@ -33,50 +33,52 @@ function copy_recoveryimage {
 }
 
 function copy_otapackage {
-	ota_out=${DISTRIBUTION}_${DEVICE_NAME}-ota-${BUILD_NUMBER}.zip
-	if ! [ -e ${ANDROID_PRODUCT_OUT}/${ota_out} ]; then
-		logb "Searching for OTA package..."
-		ota_out=`basename $(find ${ANDROID_PRODUCT_OUT} -type f -maxdepth 1 -name '*zip' | head -1)`
+	if [ "x$BUILD_TARGET" == "xotapackage" ]; then
+		ota_out=${DISTRIBUTION}_${DEVICE_NAME}-ota-${BUILD_NUMBER}.zip
+		if ! [ -e ${ANDROID_PRODUCT_OUT}/${ota_out} ]; then
+			logb "Searching for OTA package..."
+			ota_out=`basename $(find ${ANDROID_PRODUCT_OUT} -maxdepth 1 -type f -name '*zip' | head -1) 2>/dev/null`
 
-		if [ "x$ota_out" == "x" ]; then
-			logr "Failed to find ota package!!"
-			exit_error 1
+			if [ "x$ota_out" == "x" ]; then
+				logr "Failed to find ota package!!"
+				exit_error 1
+			fi
 		fi
+		logb "Found ota package $ota_out"
+
+		#define some variables
+		if [ "x${JOB_BUILD_NUMBER}" == "x" ]; then
+			rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}-${DEVICE_NAME}
+			arc_name=${DISTRIBUTION}-${ver}-$(date +%Y%m%d)-${release_type}-${DEVICE_NAME}
+		else
+			rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
+			arc_name=${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${release_type}-${DEVICE_NAME}
+		fi
+
+		#check if our correct binary exists
+		if [ -e ${BUILD_TOP}/META-INF ]; then
+			ota_bin="META-INF/com/google/android/update-binary"
+
+			logb "\t\tFound update binary..."
+			cp -dpR ${BUILD_TOP}/META-INF $BUILD_TEMP/META-INF
+			cp -ndpR ${BUILD_TOP}/META-INF ./
+			#delete the old binary
+			logb "\t\tPatching zip file unconditionally..."
+			zip -d ${ANDROID_PRODUCT_OUT}/${ota_out} ${ota_bin}
+			zip -ur ${ANDROID_PRODUCT_OUT}/${ota_out} ${ota_bin}
+		fi
+
+		#copy the zip in the background
+		logb "\t\tCopying zip image..."
+
+		# don't copy in the backgroud if we're not making the ODIN archive as well.
+		exit_on_failure rsync -v -P ${ANDROID_PRODUCT_OUT}/${ota_out} ${OUTPUT_DIR}/builds/full/${arc_name}.zip
+
+		#calculate md5sums
+		md5sums=$(md5sum ${ANDROID_PRODUCT_OUT}/${ota_out} | cut -d " " -f 1)
+
+		echo "${md5sums} ${arc_name}.zip" > ${OUTPUT_DIR}/builds/full/${arc_name}.zip.md5 || exit_error 1
 	fi
-	logb "Found ota package $ota_out"
-
-	#define some variables
-	if [ "x${JOB_BUILD_NUMBER}" == "x" ]; then
-		rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}-${DEVICE_NAME}
-		arc_name=${DISTRIBUTION}-${ver}-$(date +%Y%m%d)-${release_type}-${DEVICE_NAME}
-	else
-		rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
-		arc_name=${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${release_type}-${DEVICE_NAME}
-	fi
-
-	#check if our correct binary exists
-	if [ -e ${BUILD_TOP}/META-INF ]; then
-		ota_bin="META-INF/com/google/android/update-binary"
-
-		logb "\t\tFound update binary..."
-		cp -dpR ${BUILD_TOP}/META-INF $BUILD_TEMP/META-INF
-		cp -ndpR ${BUILD_TOP}/META-INF ./
-		#delete the old binary
-		logb "\t\tPatching zip file unconditionally..."
-		zip -d ${ANDROID_PRODUCT_OUT}/${ota_out} ${ota_bin}
-		zip -ur ${ANDROID_PRODUCT_OUT}/${ota_out} ${ota_bin}
-	fi
-
-	#copy the zip in the background
-	logb "\t\tCopying zip image..."
-
-	# don't copy in the backgroud if we're not making the ODIN archive as well.
-	exit_on_failure rsync -v -P ${ANDROID_PRODUCT_OUT}/${ota_out} ${OUTPUT_DIR}/builds/full/${arc_name}.zip
-
-	#calculate md5sums
-	md5sums=$(md5sum ${ANDROID_PRODUCT_OUT}/${ota_out} | cut -d " " -f 1)
-
-	echo "${md5sums} ${arc_name}.zip" > ${OUTPUT_DIR}/builds/full/${arc_name}.zip.md5 || exit_error 1
 }
 
 
