951e0be8f21ef58d641bba1dd0d7959986b767f3
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-04 00:54:46 +0200

    Fix substratum patch
    Change-Id: I1d30654ed763469e7f7ae17d4a8f4961b223133e

--- substratum_patch.sh
+++ substratum_patch.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 function extract_substratum_patch {
-if [ "x$ver" == "x14.1" ]; then
 logb "\t\tExtracting Substratum patch..."
 cat <<SUBS_P > ${PATCH_DIR}/substratum.patch
-diff --git a/device/samsung/msm8916-common/product/substratum.mk b/device/samsung/msm8916-common/product/substratum.mk
+diff --git a/device/samsung/msm8916-common/product/substratum1.mk b/device/samsung/msm8916-common/product/substratum1.mk
 new file mode 100644
 index 0000000..3a163fe
 --- /dev/null
-+++ b/device/samsung/msm8916-common/product/substratum.mk
++++ b/device/samsung/msm8916-common/product/substratum1.mk
 @@ -0,0 +1,3 @@
 +# Substratum
 +PRODUCT_PACKAGES += \
 +	Substratum
 SUBS_P
-fi
 }
 
 PATCH_FUNCTIONS=("${PATCH_FUNCTIONS[@]}" "extract_substratum_patch")
