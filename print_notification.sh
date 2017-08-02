d790bf6f8f7f9e72a1fb6a2e6df7443e88e2a8d4
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-02 16:34:21 -0400

    print_notification: print_end_build: Print extended info only if BUILD_URL is set
    Change-Id: I30e8a2d3fbebc15419959738f8c0814cf9c59b4f

--- print_notification.sh
+++ print_notification.sh
@@ -40,27 +40,32 @@ function print_end_build {
 	logb "Done."
 	if [ "x$SILENT" != "x1" ]; then
 		dateStr=`TZ='UTC' date +'[%H:%M:%S UTC]'`
+
 		link="${BUILD_URL}/artifact"
 
 		END_TIME=$( date +%s )
 		buildTime="%0ABuild time: $(format_time ${END_TIME} ${BUILD_START_TIME})"
 		totalTime="%0ATotal time: $(format_time ${END_TIME} ${START_TIME})"
 
-		arc_name=${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${release_type}-${DEVICE_NAME}
-		rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
-		bimg_name=bootimage-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
 
-		if [ "$BUILD_TARGET" == "recoveryimage" ]; then
-			str_rec="%0ARecovery: ${link}/builds/recovery/${DEVICE_NAME}/${rec_name}.tar"
-		elif [ "$BUILD_TARGET" == "bootimage" ]; then
-			str_boot="%0ABoot image: ${link}/builds/boot/${DEVICE_NAME}/${bimg_name}.tar"
-		elif [ "$BUILD_TARGET" == "otapackage" ]; then
-			str_rom="%0A ROM: ${link}/builds/full/${arc_name}.zip"
-			str_rec="%0A Recovery: ${link}/builds/recovery/${DEVICE_NAME}/${rec_name}.tar"
+		if [ "x$BUILD_URL" != "x" ]; then
+			arc_name=${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${release_type}-${DEVICE_NAME}
+			rec_name=${recovery_flavour}-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
+			bimg_name=bootimage-${DISTRIBUTION}-${ver}_j${JOB_BUILD_NUMBER}_$(date +%Y%m%d)_${DEVICE_NAME}
+
+			if [ "$BUILD_TARGET" == "recoveryimage" ]; then
+				str_rec="%0ARecovery: ${link}/builds/recovery/${DEVICE_NAME}/${rec_name}.tar"
+			elif [ "$BUILD_TARGET" == "bootimage" ]; then
+				str_boot="%0ABoot image: ${link}/builds/boot/${DEVICE_NAME}/${bimg_name}.tar"
+			elif [ "$BUILD_TARGET" == "otapackage" ]; then
+				str_rom="%0A ROM: ${link}/builds/full/${arc_name}.zip"
+				str_rec="%0A Recovery: ${link}/builds/recovery/${DEVICE_NAME}/${rec_name}.tar"
+			fi
+			str_changelog="%0AChangelog: ${link}/builds/full/${arc_name}.txt"
+			str_blurb="%0A%0AYou can flash boot/recovery images using ODIN or you can extract them using 7zip or tar under Linux and flash using TWRP."
 		fi
+
 		str_main="${dateStr}[${BUILD_TARGET}] ${distroTxt} ${ver} build %23${JOB_BUILD_NUMBER} for device ${DEVICE_NAME} on ${USER}@${HOSTNAME} completed successfully."
-		str_blurb="%0A%0AYou can flash boot/recovery images using ODIN or you can extract them using 7zip or tar under Linux and flash using TWRP."
-		str_changelog="%0AChangelog: ${link}/builds/full/${arc_name}.txt"
 		textStr="${str_main}${str_rom}${str_rec}${str_boot}${str_changelog}${str_blurb}${buildTime}${totalTime}"
 
 		print_to_telegram $textStr
