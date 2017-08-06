8276a4f6d9c3b4a5c860b3cdbf3e24bc3ce4c42b
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-06 14:16:32 -0400

    copy_functions: copy_recoveryimage: Don't copy recovery image loacally before compressing
    Change-Id: I1c0fa737c746f864a486a37f90fd240a6a0aa39a

--- copy_functions.sh
+++ copy_functions.sh
@@ -60,7 +60,7 @@ function copy_recoveryimage {
 		fi
 
 		logb "\n\t\tCopying recovery image...\n"
-		tar cf ${rec_name}.tar recovery.img
+		tar -C ${ANDROID_PRODUCT_OUT}/ recovery.img -c -f ${rec_name}
 		rsync_cp ${rec_name}.tar ${OUTPUT_DIR}/builds/recovery/${DEVICE_NAME}/${rec_name}.tar
 	fi
 }
