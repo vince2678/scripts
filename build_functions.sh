8f5315492ebb2f5222e151fd80541bbeb1414753
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-27 15:22:43 -0400

    Fix changelog generation
     * Always print at least some changes from each tree in the changelog
    
    Change-Id: I47be16fffeaaf6c612b08e2befb94148eea411ef

--- build_functions.sh
+++ build_functions.sh
@@ -30,50 +30,47 @@ function make_targets {
 	#fi
 }
 
+function generate_log {
+	# generate_log /path/to/git/tree
+
+	# try to use a preset time (7 days ago)
+	dates=$(date -d "`date` - 7 days" +%Y%m%d)
+	GIT_PATH=$1
+	LOG=`git -C $GIT_PATH log --decorate=full --since=$(date -d ${dates[0]} +%m-%d-%Y)`
+
+	if [ "x$LOG" == "x" ]; then
+		# output the last 6 commits
+		git -C $GIT_PATH log --decorate=full -n 6
+	else
+		git -C $GIT_PATH log --decorate=full --since=$(date -d ${dates[0]} +%m-%d-%Y)
+	fi
+}
+
 function generate_changes {
 	logb "Generating changes..."
-
-	# use a preset time (6 days ago)
-	dates=$(date -d "`date` - 6 days" +%Y%m%d)
-
-	cd ${platform_common_dir}
 
 	changelog_name=changelog-${arc_name}.txt
 
 	echo -e "\nMSM8916-COMMON\n---------\n" > ${BUILD_TEMP}/${changelog_name}
-
-	git log --decorate=full \
-		--since=$(date -d ${dates[0]} +%m-%d-%Y) >> ${BUILD_TEMP}/${changelog_name}
-
-	cd ${ANDROID_BUILD_TOP}/kernel/${vendors[0]}/${kernel_name}
+	generate_log ${platform_common_dir} >> ${BUILD_TEMP}/${changelog_name}
 
 	echo -e "\nKERNEL\n---------\n" >> ${BUILD_TEMP}/${changelog_name}
-
-	git log --decorate=full \
-		--since=$(date -d ${dates[0]} +%m-%d-%Y) >> ${BUILD_TEMP}/${changelog_name}
+	kernel_dir=${ANDROID_BUILD_TOP}/kernel/${vendors[0]}/${kernel_name}
+	generate_log ${kernel_dir} >> ${BUILD_TEMP}/${changelog_name}
 
 	if [ "x$BUILD_TARGET" == "xotapackage" ]; then
 		#generate the changes
-		cd ${ANDROID_BUILD_TOP}/device/${vendors[0]}/${DEVICE_NAME}
 
 		echo -e "\nDEVICE\n---------\n" >> ${BUILD_TEMP}/${changelog_name}
-
-		git log --decorate=full \
-			--since=$(date -d ${dates[0]} +%m-%d-%Y) >> ${BUILD_TEMP}/${changelog_name}
-
-		cd ${common_dir}
+		device_dir=${ANDROID_BUILD_TOP}/device/${vendors[0]}/${DEVICE_NAME}
+		generate_log ${device_dir} >> ${BUILD_TEMP}/${changelog_name}
 
 		echo -e "\nDEVICE-COMMON\n---------\n" >> ${BUILD_TEMP}/${changelog_name}
-
-		git log --decorate=full \
-			--since=$(date -d ${dates[0]} +%m-%d-%Y) >> ${BUILD_TEMP}/${changelog_name}
-
-		cd ${ANDROID_BUILD_TOP}/vendor/${vendors[0]}/${DEVICE_NAME}
+		generate_log ${common_dir} >> ${BUILD_TEMP}/${changelog_name}
 
 		echo -e "\nVENDOR\n---------\n" >> ${BUILD_TEMP}/${changelog_name}
-
-		git log --decorate=full \
-			--since=$(date -d ${dates[0]} +%m-%d-%Y) >> ${BUILD_TEMP}/${changelog_name}
+		vendor_dir=${ANDROID_BUILD_TOP}/vendor/${vendors[0]}/${DEVICE_NAME}
+		generate_log ${vendor_dir} >> ${BUILD_TEMP}/${changelog_name}
 
 	fi
 
