9e55ca48e165179fff87cafbda59035b3369991f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-31 10:20:54 -0400

    generate_jobs.sh: Add missing '+'
    Change-Id: I33e26a5d2c7f65bc60b46cfaf9ff05f7f33d1528

--- generate_jobs.sh
+++ generate_jobs.sh
@@ -313,7 +313,7 @@ for file in $JOB_DESC_FILES; do
 				fi
 
 				SHELL_COMMANDS="htmlroot=/var/www/ota${OTA_VER}.msm8916.com/public_html/"
-				SHELL_COMMANDS="JOB_DIR=\`${SSH} ${HOST_USER}@${HOST_NAME} &quot;find ${JENKINS_JOB_DIR} -name ${JOB_BASE_NAME} -type d | grep -i -v Promote | grep -i -v Demote&quot;\`"
+				SHELL_COMMANDS+="JOB_DIR=\`${SSH} ${HOST_USER}@${HOST_NAME} &quot;find ${JENKINS_JOB_DIR} -name ${JOB_BASE_NAME} -type d | grep -i -v Promote | grep -i -v Demote&quot;\`"
 				SHELL_COMMANDS+=${NEWLINE}
 				SHELL_COMMANDS+=${NEWLINE}
 				SHELL_COMMANDS+="${SSH} ${HOST_USER}@${HOST_NAME} &quot;find \${JOB_DIR}/lastStable/archive/builds/full -type f -execdir ln &apos;{}&apos; \${htmlroot}/builds/full/ \;&quot;"
