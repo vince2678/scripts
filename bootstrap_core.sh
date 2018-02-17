8ce138328015c65717b2c64e8e8490342d74fc65
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-02-17 17:36:16 -0500

    Supply build type and changelog days at build time for rr
    Change-Id: Ia5f33e26006d750c3159f2c07facb8fc0310b3ce

--- bootstrap_core.sh
+++ bootstrap_core.sh
@@ -219,8 +219,15 @@ function setup_env {
 	#move into the build dir
 	cd $BUILD_TOP
 
+	BUILD_TYPE=2
+	CHANGELOG_DAYS=3
+
 	#set up the environment
-	. build/envsetup.sh
+	if [ "x$DISTRIBUTION" == "xrr" ]; then
+		echo -e "${BUILD_TYPE}\n${CHANGELOG_DAYS}" | . build/envsetup.sh
+	else
+		. build/envsetup.sh
+	fi
 
 	# remove duplicate crypt_fs.
 	if [ -d ${BUILD_TOP}/device/qcom-common/cryptfs_hw ] && [ -d ${BUILD_TOP}/vendor/qcom/opensource/cryptfs_hw ]; then
